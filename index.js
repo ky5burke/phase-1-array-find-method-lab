1685621fd3a1a36a2770b6c3e6104a254e5096d2
ky5burke <ky5burke@gmail.com>
2024-02-15 23:39:39 -0600

    array methods first lab with find

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [{year: '2015', result: 'W' }, {year: '2014', result: 'N/A'}, {year: '2013', result: 'L'}]
+
+// function superbowlWin(array) {
+//     let win = array.find(function(object) {
+//       return object.result === "W";
+//     });
+// }
+
+function superbowlWin(array)  {
+    let win = array.find(function(object) {
+    return object.result === "W" ;
+     });
+     
+     if (win) {
+        return win.year;
+      } else {
+        return undefined;
+      }
+}
+
+
+console.log(superbowlWin(record));
